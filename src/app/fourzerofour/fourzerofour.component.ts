b9f86ebe13b552f2ef79557844798b2c6c53c898
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2024-02-11 13:10:45 -0600

    merge

--- src/app/fourzerofour/fourzerofour.component.ts
+++ src/app/fourzerofour/fourzerofour.component.ts
@@ -1,10 +1,20 @@
+/**
+ * Title: fourzerofour.component.ts
+ * Author: Nolan Berryhill
+ * Date: 2/11/2024
+ */
+
+// imports statements
 import { Component } from '@angular/core';
 
+// Component of selector, templateUrl, styleUrls
 @Component({
   selector: 'app-fourzerofour',
   templateUrl: './fourzerofour.component.html',
   styleUrls: ['./fourzerofour.component.css']
 })
+
+// Export FourzerofourComponent
 export class FourzerofourComponent {
 
 }
